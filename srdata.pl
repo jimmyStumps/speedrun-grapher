f3e2f3c269e8aa838ae4c258a752a1916e72bd61
AdamD2 <adam_douglas2@hotmail.com>
2016-11-20 23:42:35 +1100

    Added -H to curl request.

--- srdata.pl
+++ srdata.pl
@@ -14,8 +14,8 @@ if (@ARGV < 1) {
 }
 
 foreach my $game (@ARGV) {
-    my $html = `curl $site/$game`;
-    if ($html =~ /Could not resolve host/) {
+    my $html = `curl -H 'User-Agent: "Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"' $site/$game`;
+    if ($html =~ /Could not resolve host/ or $html =~ /$game not found./) {
         print "$game isn't on speedrun.com, check for spelling errors.\n";
         next;
     }
