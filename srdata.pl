203a3d9ae370840c0613bdb3020975e73880fa2d
AdamD2 <adam_douglas2@hotmail.com>
2016-11-20 22:57:52 +1100

    Added base script.

--- srdata.pl
+++ srdata.pl
@@ -1,15 +1,24 @@
 #!/usr/bin/perl -w
-use strict;
+#use strict;
 
-use HTML::TableExtract;
+#use HTML::TableExtract;
 
-my $te = HTML::TableExtract->new( headers => [qw(Rank Player Time Platform
-Date)] );
+#my $te = HTML::TableExtract->new( headers => [qw(Rank Player Time Platform
+#Date)] );
 
-$usage="./srdata [game]\n";
+$usage = "./srdata [game]\n";
+$site = "http://www.speedrun.com";
 
 if (@ARGV < 1) {
     die "$usage";
-} else {
-    my $game = $ARGV[0];
+}
+
+foreach my $game (@ARGV) {
+    my $html = `curl $site/$game`;
+    if ($html =~ /Could not resolve host/) {
+        print "$game isn't on speedrun.com, check for spelling errors.\n";
+        next;
+    }
+
+    print "$html";
 }
